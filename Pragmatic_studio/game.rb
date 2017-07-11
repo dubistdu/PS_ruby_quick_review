589b89d166fb87ef7ab4a3c5bc2322fa5686c104
Hyun Frantz <hyunfrantz@gmail.com>
2017-07-11 00:31:00 -0400

    refactor with module

--- Pragmatic_studio/game.rb
+++ Pragmatic_studio/game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'game_turn'
 class Game
   attr_reader :title
 
@@ -18,16 +19,8 @@ class Game
       puts player
     end
     @players.each do|player|
-      die = Die.new
-      number_rolled = die.roll
-      case number_rolled
-      when 1..2
-        player.blam
-      when 3..4
-        puts "#{player.name} is skipped"
-      else player.w00t
-      end
-        puts player
+      GameTurn.take_turn(player)
+      puts player
     end
   end
 end
---
+++ Pragmatic_studio/game_turn.rb
@@ -0,0 +1,13 @@
+module GameTurn
+  def self.take_turn(player)
+      die = Die.new
+      number_rolled = die.roll
+      case number_rolled
+      when 1..2
+        player.blam
+      when 3..4
+        puts "#{player.name} is skipped"
+      else player.w00t
+      end
+    end
+  end
